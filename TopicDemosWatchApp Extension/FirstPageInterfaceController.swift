4974d8b9eb812b911aaeb373a568413477c0b213
Max Wang <max.wang.cs@gmail.com>
2017-02-17 12:23:12 -0800

    picker & context menu

--- TopicDemosWatchApp Extension/FirstPageInterfaceController.swift
+++ TopicDemosWatchApp Extension/FirstPageInterfaceController.swift
@@ -13,21 +13,32 @@ import Foundation
 class FirstPageInterfaceController: WKInterfaceController {
     @IBOutlet var label: WKInterfaceLabel!
     @IBOutlet var inlineMovie: WKInterfaceInlineMovie! //OS 3.0
+    @IBOutlet var picker: WKInterfacePicker!
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        //set up data
         if let dict = context as? [String: String] {
              label.setText(dict["key"])
         }
-        
+        //setup video
         if let videoURL = Bundle.main.url(forResource: "SampleVideo_360x240_2mb", withExtension: "mp4") {
             print("find mp4")
             inlineMovie.setMovieURL(videoURL)
             //inlineMovie.playFromBeginning()
         }
-        // Configure interface objects here.
+        //setup picker -> TODO: percent circle adjusted by crown
+        //https://www.bignerdranch.com/blog/watchkit-2-hardware-bits-using-the-digital-crown/
+        var items = [WKPickerItem]()
+        for i in 0...2 {
+        let item = WKPickerItem()
+            item.title = "title" + String(i)
+            item.caption = "caption" + String(i)
+            items.append(item)
+        }
+        picker.setItems(items)
+        picker.focus()
     }
-    
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
@@ -41,12 +52,27 @@ class FirstPageInterfaceController: WKInterfaceController {
     @IBAction func backClicked() {
        self.pop()
     }
+    
     @IBAction func g1Clicked() {
         self.label.setText("g1")
     }
     
     @IBAction func g2Clicked() {
         self.label.setText("g2")
+    }
+    
+    //MARK: Picker: If your app should respond only to the item that the user selected
+    override func pickerDidSettle(_ picker: WKInterfacePicker) {
+        print("picker selected: \(picker)")
+        //get index from picker Action and do expensive op here!!! -> Cannot get index here directly
+    }
+    @IBAction func pickerAction(_ index: Int) {
+        print("picker Scrolled to \(index)")
+    }
+    //MARK: Context Menu
+    
+    @IBAction func firstMenuItemClicked() {
+        print("firstMenuItemClicked")
     }
     
 }
