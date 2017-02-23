cbaeedef816416e9a6651b2e9cb1d7402a67e50e
Max Wang <mwang@mwang-m01.ad.savagebeast.com>
2017-02-22 23:40:49 -0800

    container controller programmatically

---
+++ TopicDemos/ContainerController/ChildViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChildViewController.swift
+//  TopicDemos
+//
+//  Created by Max Wang on 2/22/17.
+//  Copyright © 2017 Sida Wang. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
