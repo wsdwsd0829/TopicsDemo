2531b7443d50491d3a263436b416f80734cabdaa
Max Wang <max.wang.cs@gmail.com>
2017-04-16 23:36:45 -0700

    fix blooth scan need power on

---
+++ TopicDemos/SecondTab/CustomPresentAnimate/AnimatorObject.swift
@@ -0,0 +1,13 @@
+//
+//  AnimatorObject.swift
+//  TopicDemos
+//
+//  Created by Sida Wang on 4/16/17.
+//  Copyright © 2017 Sida Wang. All rights reserved.
+//
+
+import UIKit
+
+class AnimatorObject: UIViewControllerAnimatedTransitioning {
+
+}
---
+++ TopicDemos/SecondTab/CustomPresentAnimate/InteractiveObject.swift
@@ -0,0 +1,13 @@
+//
+//  InteractiveObject.swift
+//  TopicDemos
+//
+//  Created by Sida Wang on 4/16/17.
+//  Copyright © 2017 Sida Wang. All rights reserved.
+//
+
+import UIKit
+
+class InteractiveObject: UIPercentDrivenInteractiveTransition {
+
+}
