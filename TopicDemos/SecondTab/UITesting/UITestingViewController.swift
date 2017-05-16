b26f846644f05bbdd5b8b803abb1b9fa646d8d41
Max Wang <mwang@pandora.com>
2017-05-16 13:43:43 -0700

    add delete row functin together with gesture recgonizer

--- TopicDemos/SecondTab/UITesting/UITestingViewController.swift
+++ TopicDemos/SecondTab/UITesting/UITestingViewController.swift
@@ -8,24 +8,48 @@
 
 import UIKit
 
-class UITestingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class UITestingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
+    var tgr:UITapGestureRecognizer!
+    
+    var animals = ["cat", "dog", "snake", "mouse", "horse", "bull", "tiger", "rabbit", "dragon", "sheep", "monkey", "chicken", "dog", "pig"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.edgesForExtendedLayout = [];
-        let tgr = UITapGestureRecognizer(target: self, action: #selector(tappedInView(_:)))
+         tgr = UITapGestureRecognizer(target: self, action: #selector(tappedInView(_:)))
         view.addGestureRecognizer(tgr)
-        // Do any additional setup after loading the view.
+        tgr.delegate = self
+//!!!http://stackoverflow.com/questions/8192480/uitapgesturerecognizer-breaks-uitableview-didselectrowatindexpath
+        tgr.cancelsTouchesInView = false  //will pass down to view from tableView and do not block tableView from receive it's own touch event to trigger didSelectRow 
+        
+        let bbi = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editClicked))
+        self.navigationItem.rightBarButtonItem = bbi
+
+        print(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = true
+        tableView.allowsSelectionDuringEditing = true
+        tableView.isUserInteractionEnabled = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ /*  //Not working
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if gestureRecognizer == tgr {
+            if type(of: touch.view) == UITableViewCell.self {
+                return false
+            }
+        }
+        return true
+    }
+  */
+    func editClicked() {
+        tableView.setEditing(!tableView.isEditing, animated: true)
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
@@ -35,32 +59,47 @@ class UITestingViewController: UIViewController, UITableViewDataSource, UITableV
     func tappedInView(_ sender: AnyObject) {
         textField.resignFirstResponder()
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50;
+        return animals.count;
+
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(tableView)
+
         var cell:UITableViewCell?
         let cellId = "UITestingCell"
         if let mycell = tableView.dequeueReusableCell(withIdentifier: cellId) {
             cell = mycell
-            cell?.textLabel?.text = "Cell row index: \(indexPath.row)"
+            cell?.textLabel?.text = "Cell row index: \(indexPath.row) \(animals[indexPath.row])"
         } else {
             cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
-            cell?.textLabel?.text = "Cell row index: \(indexPath.row)"
-
+            cell?.textLabel?.text = "Cell row index: \(indexPath.row) \(animals[indexPath.row])"
         }
         return cell!
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
-    */
 
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            animals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+        }
+        tableView.reloadData()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+    }
 }
+
+
+
+
+
+
+
+
