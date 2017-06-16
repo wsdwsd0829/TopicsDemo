c3712743b61ec8c497988aec715d025c8e157019
Max Wang <mwang@pandora.com>
2017-06-15 21:14:54 -0700

    add sequence and functional tests

--- Playgrounds/Collection.playground/Contents.swift
+++ Playgrounds/Collection.playground/Contents.swift
@@ -71,3 +71,65 @@ for i in threeToGo2 {
 }
 print(threeToGo2.count)
 
+
+struct ReverseIterator: IteratorProtocol {
+    var index: Int
+    init<T>(array: [T]) {
+        self.index = array.endIndex - 1
+    }
+    mutating func next() -> Int? {
+        defer {
+            index -= 1
+        }
+        guard index >= 0 else { return nil }
+       return index
+    }
+}
+
+print("-----------")
+
+var arr = [1,2,3,4,5]
+var reverseIterator = ReverseIterator(array: arr)
+while let index = reverseIterator.next() {
+    print(arr[index])
+}
+struct ReverseIndices<T>: Sequence {
+    let array: [T]
+    init(array: [T]) {
+        self.array = array
+    }
+    
+    func makeIterator() -> ReverseIterator {
+        return ReverseIterator(array: array)
+    }
+    
+    //same as map
+    func mymap<T>(_ transform: (Int) throws -> T) rethrows -> [T] {
+        var iter = makeIterator()
+        var result = [T]()
+        while let index = iter.next(), let elem = try? transform(index) {
+            result.append(elem)
+        }
+        return result
+    }
+}
+
+var arr1 = ["one", "two", "three"]
+var reverseSequence = ReverseIndices(array: arr1)
+let elemSequence = reverseSequence.mymap {
+    arr1[$0]
+}
+
+for elem in elemSequence {
+    print(elem)
+}
+
+let res = (1...10).lazy.filter{ $0 % 3 == 0 }.map { $0 * $0 }
+print(res)
+for i in res {
+    print(i)
+}
+
+
+
+
