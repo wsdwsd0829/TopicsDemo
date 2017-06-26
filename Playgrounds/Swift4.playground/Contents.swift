148fe4dc7dd9da614e326eae926b45dd3f7a845c
Max Wang <max.wang.cs@gmail.com>
2017-06-25 17:02:31 -0700

    swift 4 practice

---
+++ Playgrounds/Swift4.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let galaxy = "Milky Way 🐮"
+for char in galaxy {
+  print(char)
+}
+print(galaxy.characters)
+
+var family = "👩"
+family += "\u{200D}👩"
+family.count
+
+family += "\u{200D}👧"
+family.count
+
+family += "\u{200D}👦"
+family.count
+
+
+
