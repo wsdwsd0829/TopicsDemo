061e2100b782fa38bad9e14c55fe75215b7fc0ae
Max Wang <max.wang.cs@gmail.com>
2017-06-26 23:34:06 -0700

    resolve #5 bluepill to run multiple simulator

---
+++ Playgrounds/ToolsPlayground.playground/Pages/BluePill.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+//blue pill project: ~/max/ps/demos/Tools/
+//https://github.com/linkedin/bluepill/
+
+//e.g.
+// ./bluepill -a /Users/sidawang/Library/Developer/Xcode/DerivedData/TopicDemos-elzqazbjnpqdgpekmerfxtccxjey/Build/Products/Debug-iphonesimulator/TopicDemos.app -s /Users/sidawang/max/ps/mydemos/TopicDemos/TopicDemos.xcodeproj/xcuserdata/sidawang.xcuserdatad/xcschemes/TopicDemos.xcscheme -o ./output/
+
+//above command ref: https://engineering.linkedin.com/blog/2017/01/open-sourcing-bluepill--run-ios-tests-in-multiple-simulators
+//./bluepill -a ./Sample.app -s ./SampleAppTestScheme.xcscheme -o ./output/
+
+//!!! need to edit scheme to mark all test targets "run" //default not
+//!!! notice scheme & app in different places
+
+//
