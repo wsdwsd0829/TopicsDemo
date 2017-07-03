c25d21bae26345038b757e4f146b68e13c4e07ad
Max Wang <max.wang.cs@gmail.com>
2017-07-03 16:36:58 -0700

    add readme travis, fix tests

--- Playgrounds/ToolsPlayground.playground/Pages/TravisCI.xcplaygroundpage/Contents.swift
+++ Playgrounds/ToolsPlayground.playground/Pages/TravisCI.xcplaygroundpage/Contents.swift
@@ -3,6 +3,9 @@
 import Foundation
 
 //https://docs.travis-ci.com/user/getting-started/
+//https://travis-ci.org/wsdwsd0829/TopicsDemo/pull_requests
 
+
+//https://www.raywenderlich.com/109418/travis-ci-tutorial
 
 //: [Next](@next)
---
+++ README.md
@@ -0,0 +1,2 @@
+####Master branch build status: 
+![](https://travis-ci.org/wsdwsd0829/TopicDemos.svg?branch=master)
